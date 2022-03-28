a3b46367bd57d106e9ff1cf0cb2a0743033a1c58
Joshua Medina <98380182+joshgcash@users.noreply.github.com>
2022-03-28 20:38:53 +0800

    Create HW1-3.js

---
+++ HW1-3.js
@@ -0,0 +1,12 @@
+//Javascript
+//HW1-3.js
+//Reverse
+
+function reverse(string){
+  return string = [...string].reverse().join("")
+}
+
+const string = "joshua"
+const reverseString = reverse(string)
+
+console.log(reverseString)
